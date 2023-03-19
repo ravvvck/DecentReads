029d20b839e5d07d0f0459c2e0da74f59f0c5e9f
rafalkwiatek1 <rafal.kwiatek@edu.uekat.pl>
2023-03-19 20:46:10 +0100

    Update dependencies

---
+++ DecentReads.Api/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using DecentReads.Api.Mapping;
+using MediatR;
+
+namespace DecentReads.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddMappings();
+
+            return services;
+        }
+
+    }
+}
--- DecentReads.Api/Program.cs
+++ DecentReads.Api/Program.cs
@@ -1,43 +1,30 @@
+using DecentReads.Api;
+using DecentReads.Api.Middleware;
 using DecentReads.Application;
-using DecentReads.Persistence;
+using DecentReads.Infrastructure;
 using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.ConfigureApplicationServices();
-//builder.Services.ConfigureInfrastructureServices(Configuration);
-builder.Services.ConfigurePersistenceServices(builder.Configuration);
-
-builder.Services.AddCors(options =>
 {
-    options.AddPolicy("FrontEndClient", builder =>
-    builder.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowCredentials());
-});
+    //builder.Services.AddTransient<ErrorHandlingMiddleware>();
+    builder.Services.AddApplication()
+        .AddInfrastructure(builder.Configuration)
+        .AddPresentation();
+    builder.Services.AddControllers();
 
 
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+}
 
 
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseCors("FrontEndClient");
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
