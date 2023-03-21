12f41011da5361337c91c777fbcdd16b454ecd47
rafalkwiatek1 <rafal.kwiatek@edu.uekat.pl>
2023-03-21 20:00:14 +0100

    Delete unused files

---
+++ DecentReads.Application/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using DecentReads.Application.Common.Behaviors;
+using DecentReads.Domain.Users;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecentReads.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(DependencyInjection).Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+            services.AddHttpContextAccessor();
+
+            return services;
+        }
+
+    }
+}
