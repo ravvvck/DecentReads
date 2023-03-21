ad26f31d2bab61718f579b629ed9fdb4e8e494ac
rafalkwiatek1 <rafal.kwiatek@edu.uekat.pl>
2023-03-21 19:48:06 +0100

    Add book mapping

---
+++ DecentReads.Api/Mapping/BookMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DecentReads.Application.DTOs.Book;
+using DecentReads.Domain.Books;
+
+namespace DecentReads.Api.Mapping
+{
+    public class BookMapping : Profile
+    {
+        public BookMapping()
+        {
+            CreateMap<Book, BookDto>()
+           .ForMember(m => m.Author, c => c.MapFrom(s => s.Author.FullName));
+        }
+    }
+}
