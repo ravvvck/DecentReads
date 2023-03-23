a78f2e42a90b78606d0d7eab7e1e93cde1868e78
rafalkwiatek1 <rafal.kwiatek@edu.uekat.pl>
2023-03-23 11:22:17 +0100

    Add logging behavior

---
+++ DecentReads.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,49 @@
+﻿using DecentReads.Application.Common.Interfaces.Services;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecentReads.Application.Common.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+        private readonly ICurrentUserService _currentUserService;
+
+        public LoggingBehavior(ILogger<LoggingBehavior< TRequest, TResponse>> logger, ICurrentUserService currentUserService)
+        {
+            _logger = logger;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var userId = _currentUserService.UserId ?? string.Empty;
+            string? userName = string.Empty;
+
+
+
+            _logger.LogInformation("Request: {Name} {@UserId} {@UserName} {@Request}",
+                requestName, userId, userName, request);
+
+
+
+            var result = await next();
+
+            
+            
+
+            return result;
+        }
+
+        
+    }
+}
---
+++ DecentReads.Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,23 @@
+﻿using DecentReads.Application.Common.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecentReads.Infrastructure.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
